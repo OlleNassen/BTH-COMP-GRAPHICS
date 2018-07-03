7a16bf91bf6155ad541e3b688fd84dd94188d48f
Deadvard <edvardleiborn@telia.com>
2018-07-03 11:40:54 +0200

    small change

--- BTH COMP GRAPHICS/include/directional_light.hpp
+++ BTH COMP GRAPHICS/include/directional_light.hpp
@@ -2,6 +2,9 @@
 #define DIRECTIONAL_LIGHT_HPP
 
 #include "shader.hpp"
+#include "camera.hpp"
+#include "frame_buffer.hpp"
+#include "texture.hpp"
 
 class directional_light
 {
@@ -15,11 +18,15 @@ public:
     void bind(const shader& shader);
 
 private:
+    glm::mat4 light_space_matrix;
     glm::vec3 direction;
     glm::vec3 ambient;
     glm::vec3 diffuse;
     glm::vec3 specular;
 
+    camera shadow_camera;
+    texture shadow_texture;
+    frame_buffer shadow_buffer;
 };
 
 #endif // DIRECTIONAL_LIGHT_HPP
