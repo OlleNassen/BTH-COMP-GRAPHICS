251e62dd695a44f383eec0a145d296949c714cc2
Deadvard <edvardleiborn@telia.com>
2018-07-03 18:18:07 +0200

    scene node added

---
+++ BTH COMP GRAPHICS/include/scene_node.hpp
@@ -0,0 +1,33 @@
+#ifndef SCENE_NODE_HPP
+#define SCENE_NODE_HPP
+
+#include <vector>
+#include <glm/glm.hpp>
+#include "shader.hpp"
+
+class scene_node
+{
+public:
+    scene_node();
+    virtual ~scene_node();
+    void attach_child(scene_node* child);
+
+    void update(float delta_time);
+    void render(const shader& shader) const;
+
+private:
+    std::vector<scene_node*> children;
+    glm::mat4 transform;
+
+    void update(float delta_time, glm::mat4& world_transform);
+    void render(const shader& shader, glm::mat4& world_transform) const;
+
+    virtual void update_current(float delta_time,
+        const glm::mat4& world_transform, glm::mat4& transform);
+    virtual void render_current(const shader& shader, const glm::mat4& world_transform) const;
+
+    void update_children(float delta_time, glm::mat4& world_transform);
+    void render_children(const shader& shader, glm::mat4& world_transform) const;
+};
+
+#endif // SCENE_NODE_HPP
---
+++ BTH COMP GRAPHICS/src/scene_node.cpp
@@ -0,0 +1,77 @@
+#include "scene_node.hpp"
+
+scene_node::scene_node()
+    : children()
+    , transform(1.0f)
+{
+
+}
+
+scene_node::~scene_node()
+{
+    for(auto* child : children)
+    {
+        delete child;
+    }
+}
+
+void scene_node::attach_child(scene_node* child)
+{
+    children.push_back(child);
+}
+
+void scene_node::update(float delta_time)
+{
+    glm::mat4 world_transform(1.0f);
+    update(delta_time, world_transform);
+}
+
+void scene_node::render(const shader& shader) const
+{
+    glm::mat4 world_transform(1.0f);
+    render(shader, world_transform);
+}
+
+void scene_node::update(float delta_time, glm::mat4& world_transform)
+{
+    world_transform *= transform;
+
+    update_current(delta_time,
+        world_transform, transform);
+    update_children(delta_time, world_transform);
+}
+
+void scene_node::render(const shader& shader, glm::mat4& world_transform) const
+{
+    world_transform *= transform;
+
+    render_current(shader, world_transform);
+	render_children(shader, world_transform);
+}
+
+void scene_node::update_current(float delta_time,
+    const glm::mat4& world_transform, glm::mat4& transform)
+{
+
+}
+
+void scene_node::render_current(const shader& shader, const glm::mat4& world_transform) const
+{
+
+}
+
+void scene_node::update_children(float delta_time, glm::mat4& world_transform)
+{
+    for(auto* child : children)
+    {
+        child->update(delta_time, world_transform);
+    }
+}
+
+void scene_node::render_children(const shader& shader, glm::mat4& world_transform) const
+{
+    for(const auto* child : children)
+    {
+        child->render(shader, world_transform);
+    }
+}
