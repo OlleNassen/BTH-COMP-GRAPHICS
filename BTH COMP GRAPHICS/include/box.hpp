8f36a1d560df6b1caea18cc341888c0bd7b3fb80
Deadvard <edvardleiborn@telia.com>
2018-07-04 10:18:15 +0200

    fix private

--- BTH COMP GRAPHICS/include/box.hpp
+++ BTH COMP GRAPHICS/include/box.hpp
@@ -11,13 +11,14 @@ public:
     box();
     virtual ~box();
 
+private:
      virtual void update_current(float delta_time,
         const glm::mat4& world_transform, glm::mat4& transform) override;
 
     virtual void render_current(const shader& shader,
         const glm::mat4& world_transform) const override;
 
-private:
+
     buffer box_buffer;
     vertex_array box_array;
 };
