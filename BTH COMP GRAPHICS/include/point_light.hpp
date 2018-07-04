7b55e74fc94b7ed3b8a050f7d8d5180fe3fc9856
OlleNassen <olle_sz@hotmail.com>
2018-07-04 17:15:29 +0200

    point light

---
+++ BTH COMP GRAPHICS/include/point_light.hpp
@@ -0,0 +1,28 @@
+#ifndef POINT_LIGHT_HPP
+#define POINT_LIGHT_HPP
+
+#include "scene_node.hpp"
+#include "buffer.hpp"
+#include "vertex_array.hpp"
+#include "texture.hpp"
+
+class point_light: public scene_node
+{
+public:
+	point_light();
+	virtual ~point_light();
+
+private:
+	glm::vec3 ambient;
+	glm::vec3 diffuse;
+	glm::vec3 specular;
+
+	float constant;
+	float linear;
+	float quadratic;
+
+	virtual void update_current(float delta_time,
+		const glm::mat4& world_transform, glm::mat4& transform) override;
+};
+
+#endif
---
+++ BTH COMP GRAPHICS/src/point_light.cpp
@@ -0,0 +1,13 @@
+#include "point_light.hpp"
+
+point_light::point_light()
+{
+}
+
+point_light::~point_light()
+{
+}
+
+void point_light::update_current(float delta_time, const glm::mat4 & world_transform, glm::mat4 & transform)
+{
+}
