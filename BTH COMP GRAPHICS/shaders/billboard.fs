86325695e5dd8de2935aaeaf84421f7e853c563d
OlleNassen <olle_sz@hotmail.com>
2018-07-06 11:44:30 +0200

    bug fix

--- BTH COMP GRAPHICS/shaders/billboard.fs
+++ BTH COMP GRAPHICS/shaders/billboard.fs
@@ -6,10 +6,10 @@ in vec3 vs_color;
 
 out vec4 fragColor;
 
-uniform sampler2D image;
+uniform sampler2D diffuseMap;
 uniform vec4 color;
 
 void main()
 {
-   	fragColor = texture2D(image, vs_texcoord) * color;
+   	fragColor = texture2D(diffuseMap, vs_texcoord) * vec3(1,0.1,0.1);
 }
