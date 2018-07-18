e25666bd619ed718d078b3e958e44c7c510579a7
Deadvard <edvardleiborn@telia.com>
2018-07-18 19:28:17 +0200

    still need heirarchy fix

--- BTH COMP GRAPHICS/src/model.cpp
+++ BTH COMP GRAPHICS/src/model.cpp
@@ -102,8 +102,6 @@ void load_parent_indices(const aiNode& node, std::vector<std::string>& names)
 void load_parent_indices(const aiNode* node, skeleton& joints)
 {
     std::vector<std::string> names;
-
-    names.emplace_back(node->mName.C_Str());
     for (auto i = 0u; i < node->mNumChildren; i++)
 	{
         load_parent_indices(*node->mChildren[i], names);
