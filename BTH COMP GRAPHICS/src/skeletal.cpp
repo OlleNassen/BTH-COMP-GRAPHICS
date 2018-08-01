7de09944d3ef7cbe531c1430d103b17e0c59a52b
OlleNassen <olle_sz@hotmail.com>
2018-08-01 14:10:29 +0200

    for loop

--- BTH COMP GRAPHICS/src/skeletal.cpp
+++ BTH COMP GRAPHICS/src/skeletal.cpp
@@ -14,19 +14,15 @@ skeletal::skeletal()
 		0.0f, 1.0f, 0.f, 0.0f, 1.0f
 	};
 
-	objects[0].bind();
-	buffers[0].data(sizeof(vertices), &vertices[0], GL_STATIC_DRAW);
-	objects[0].attribute_pointer(0, 3, GL_FLOAT,
-		GL_FALSE, 5 * sizeof(float), nullptr);
-	objects[0].attribute_pointer(1, 2, GL_FLOAT,
-		GL_FALSE, 5 * sizeof(float), scene::buffer_offset<float>(3u));
-
-	objects[1].bind();
-	buffers[1].data(sizeof(vertices), &vertices[0], GL_STATIC_DRAW);
-	objects[1].attribute_pointer(0, 3, GL_FLOAT,
-		GL_FALSE, 5 * sizeof(float), nullptr);
-	objects[1].attribute_pointer(1, 2, GL_FLOAT,
-		GL_FALSE, 5 * sizeof(float), scene::buffer_offset<float>(3u));
+	for (int i = 0; i < NUM_OBJECTS; i++)
+	{
+		objects[i].bind();
+		buffers[i].data(sizeof(vertices), &vertices, GL_STATIC_DRAW);
+		objects[i].attribute_pointer(0, 3, GL_FLOAT,
+			GL_FALSE, 5 * sizeof(float), nullptr);
+		objects[i].attribute_pointer(1, 2, GL_FLOAT,
+			GL_FALSE, 5 * sizeof(float), scene::buffer_offset<float>(3u));
+	}
 
 	for (auto& transf : transforms)
 	{
@@ -43,11 +39,14 @@ skeletal::skeletal()
 skeletal::~skeletal()
 {
 }
-
+#include <iostream>
 void skeletal::render_current(const shader& shader,
 	const glm::mat4& world_transform)
 {
-	//transforms[1] = transforms[1] * transforms[0];
+	//transforms[1] *= transforms[0];
+	transforms[1] *= glm::mat4_cast(rotations[0]);
+
+	std::cout << transforms[1][3][0] << " " << transforms[1][3][1] << " " << transforms[1][3][2] << '\n';
 
 	transforms[0][3][1] = glm::sin(1*glfwGetTime());
 	rotations[0] = glm::rotate(rotations[0], 0.00001f, glm::vec3(1, 0, 0));
