abdce8f81aa787d0c1abb572f95154a486ab291d
OlleNassen <olle_sz@hotmail.com>
2018-08-01 12:25:46 +0200

    upload

--- BTH COMP GRAPHICS/include/skeletal.hpp
+++ BTH COMP GRAPHICS/include/skeletal.hpp
@@ -5,21 +5,24 @@
 #include "buffer.hpp"
 #include "node.hpp"
 #include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
 #include <GLFW/glfw3.h>
 
 class skeletal
 {
-static const unsigned int NUM_OBJECTS = 2;
-private:
-	std::array<vertex_array, NUM_OBJECTS> objects;
-	std::array<buffer, NUM_OBJECTS> buffers;
-	std::array<glm::mat4, NUM_OBJECTS>transforms;
-	std::array<glm::quat, NUM_OBJECTS>rotations;
 public:
 	skeletal();
 	~skeletal();
 	void render_current(const shader& shader,
 		const glm::mat4& world_transform);
+
+private:
+	static constexpr unsigned int NUM_OBJECTS = 2;
+	std::array<vertex_array, NUM_OBJECTS> objects;
+	std::array<buffer, NUM_OBJECTS> buffers;
+	std::array<glm::mat4, NUM_OBJECTS>transforms;
+	std::array<glm::quat, NUM_OBJECTS>rotations;
+
 };
 
 #endif
