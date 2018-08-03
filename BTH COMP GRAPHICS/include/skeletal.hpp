f0bd63ac5e5edab14368c8890f14a692f9e4540d
OlleNassen <olle_sz@hotmail.com>
2018-08-03 13:44:47 +0200

    added scale

--- BTH COMP GRAPHICS/include/skeletal.hpp
+++ BTH COMP GRAPHICS/include/skeletal.hpp
@@ -112,9 +112,15 @@ public:
 		skeletal_node* body = new skeletal_node(cube, glm::vec4(1, 0, 0, 1));    //Red!
 		body->set_model_scale(glm::vec3(10, 15, 5));
 		glm::mat4 tran(1.f);
+		//Transform
 		tran[3][0] = 0.f;
-		tran[3][1] = 35.f;
+		tran[3][1] = 20.f;
 		tran[3][2] = 0.f;
+		//Scale
+		tran[0][0] = body->get_model_scale().x;
+		tran[1][1] = body->get_model_scale().y;
+		tran[2][2] = body->get_model_scale().z;
+
 		body->set_transform(tran);
 		add_child(body);
 
@@ -123,8 +129,12 @@ public:
 		head->set_model_scale(glm::vec3(5, 5, 5));
 		glm::mat4 tran1(1.f);
 		tran1[3][0] = 0.f;
-		tran1[3][1] = 30.f;
+		tran1[3][1] = 35.f;
 		tran1[3][2] = 0.f;
+		//Scale
+		tran1[0][0] = head->get_model_scale().x;
+		tran1[1][1] = head->get_model_scale().y;
+		tran1[2][2] = head->get_model_scale().z;
 		head->set_transform(tran1);
 		add_child(head);
 
@@ -135,6 +145,10 @@ public:
 		tran2[3][0] = -12.f;
 		tran2[3][1] = 30.f;
 		tran2[3][2] = -1.f;
+		//Scale
+		tran2[0][0] = left_arm->get_model_scale().x;
+		tran2[1][1] = left_arm->get_model_scale().y;
+		tran2[2][2] = left_arm->get_model_scale().z;
 		left_arm->set_transform(tran2);
 		add_child(left_arm);
 
@@ -145,6 +159,10 @@ public:
 		tran3[3][0] = 12.f;
 		tran3[3][1] = 30.f;
 		tran3[3][2] = -1.f;
+		//Scale
+		tran3[0][0] = right_arm->get_model_scale().x;
+		tran3[1][1] = right_arm->get_model_scale().y;
+		tran3[2][2] = right_arm->get_model_scale().z;
 		right_arm->set_transform(tran3);
 		add_child(right_arm);
 
@@ -155,6 +173,10 @@ public:
 		tran4[3][0] = -8.f;
 		tran4[3][1] = 0.f;
 		tran4[3][2] = 0.f;
+		//Scale
+		tran4[0][0] = left_leg->get_model_scale().x;
+		tran4[1][1] = left_leg->get_model_scale().y;
+		tran4[2][2] = left_leg->get_model_scale().z;
 		left_leg->set_transform(tran4);
 		add_child(left_leg);
 
@@ -165,6 +187,10 @@ public:
 		tran5[3][0] = 8.f;
 		tran5[3][1] = 0.f;
 		tran5[3][2] = 0.f;
+		//Scale
+		tran5[0][0] = right_leg->get_model_scale().x;
+		tran5[1][1] = right_leg->get_model_scale().y;
+		tran5[2][2] = right_leg->get_model_scale().z;
 		right_leg->set_transform(tran5);
 		add_child(right_leg);
 
