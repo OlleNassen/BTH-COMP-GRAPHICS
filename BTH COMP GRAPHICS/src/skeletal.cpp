aa241cdcb0b4b43b6d64d1c7f739606080b5117c
Deadvard <edvardleiborn@telia.com>
2018-08-03 12:30:24 +0200

    moved virtual func to cpp

--- BTH COMP GRAPHICS/src/skeletal.cpp
+++ BTH COMP GRAPHICS/src/skeletal.cpp
@@ -23,3 +23,42 @@ void skeletal::render_current(const shader& shader,
 	glBindVertexArray(0);
 	*/
 }
+void skeletal_node::update(float delta_time)
+{
+    if (parent)
+    {
+        world_transform = parent->world_transform * transform;
+    }
+    else
+    {
+        world_transform = transform;
+    }
+
+    for (auto& child : children)
+    {
+        child->update(delta_time);
+    }
+}
+
+void cube_robot::update(float delta_time)
+{
+    std::cout << "NO";
+    std::cout << transform[3][0] << '\n';
+    transform = transform *
+        glm::rotate(transform, delta_time / 10.0f,
+            glm::vec3(0.f, 1.f, 0.f));
+
+    head->set_transform(head->get_transform() *
+        glm::rotate(head->get_transform(), -delta_time / 10.0f,
+            glm::vec3(0.f, 1.f, 0.f)));
+
+    left_arm->set_transform(left_arm->get_transform() *
+        glm::rotate(left_arm->get_transform(), -delta_time / 10.0f,
+            glm::vec3(1.f, 0.f, 0.f)));
+
+    right_arm->set_transform(right_arm->get_transform() *
+        glm::rotate(right_arm->get_transform(), delta_time / 10.0f,
+            glm::vec3(1.f, 0.f, 0.f)));
+
+    skeletal_node::update(delta_time);
+}
