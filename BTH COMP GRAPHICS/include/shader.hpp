9a8e6069b4a425e88c77a90a3c0664d32055a842
Deadvard <edvardleiborn@telia.com>
2018-08-14 12:26:07 +0200

    column 80

--- BTH COMP GRAPHICS/include/shader.hpp
+++ BTH COMP GRAPHICS/include/shader.hpp
@@ -26,22 +26,31 @@ public:
     void uniform(const std::string& name, const glm::vec2& value) const;
     void uniform(const std::string& name, const glm::vec3& value) const;
     void uniform(const std::string& name, const glm::vec4& value) const;
-    void uniform(const std::string& name, const glm::vec<4, int, glm::highp>& value) const;
+    void uniform(const std::string& name,
+        const glm::vec<4, int, glm::highp>& value) const;
     void uniform(const std::string& name, const glm::mat4& value) const;
 
-    void uniform(const std::string& name, const std::vector<int>& value) const;
-    void uniform(const std::string& name, const std::vector<float>& value) const;
-    void uniform(const std::string& name, const std::vector<glm::vec2>& value) const;
-    void uniform(const std::string& name, const std::vector<glm::vec3>& value) const;
-    void uniform(const std::string& name, const std::vector<glm::vec4>& value) const;
-    void uniform(const std::string& name, const std::vector<glm::mat4>& value) const;
-    void uniform(const std::string& name, const std::array<glm::mat4, 50>& value) const;
+    void uniform(const std::string& name,
+        const std::vector<int>& value) const;
+    void uniform(const std::string& name,
+        const std::vector<float>& value) const;
+    void uniform(const std::string& name,
+        const std::vector<glm::vec2>& value) const;
+    void uniform(const std::string& name,
+        const std::vector<glm::vec3>& value) const;
+    void uniform(const std::string& name,
+        const std::vector<glm::vec4>& value) const;
+    void uniform(const std::string& name,
+        const std::vector<glm::mat4>& value) const;
+    void uniform(const std::string& name,
+        const std::array<glm::mat4, 50>& value) const;
 
 private:
     unsigned int id;
 
     std::string load(const std::string& path) const;
-    unsigned int create(unsigned int shader_type, const char* shader_code) const;
+    unsigned int create(unsigned int shader_type,
+        const char* shader_code) const;
 };
 
 #endif // SHADER_HPP
