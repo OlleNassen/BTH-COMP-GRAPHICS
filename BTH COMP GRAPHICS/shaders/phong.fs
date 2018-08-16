d0b64babbf7ee602ee4e38f85613b2f8c4e9c1de
OlleNassen <olle_sz@hotmail.com>
2018-08-16 13:32:46 +0200

    phong update

--- BTH COMP GRAPHICS/shaders/phong.fs
+++ BTH COMP GRAPHICS/shaders/phong.fs
@@ -14,16 +14,16 @@ uniform vec3 view_position; // World cordinates
 void main()
 {
 	vec3 normalized_normal = normalize(normal);
-	vec3 light_direction = normalize(light_pos - fragment_pos);
+	vec3 light_direction = normalize(fragment_pos - light_pos);
 
 	vec3 view_direction = normalize(view_position - fragment_pos);
-	vec3 reflection_direction = reflect(-light_direction, normalized_normal); //Change direction
+	vec3 reflection_direction = reflect(light_direction, normalized_normal); //Change direction
 
 	float specular_value = 0.5;
 	float spec = pow(max(dot(view_direction, reflection_direction), 0.0), 32);
 	vec3 specular = light_color * spec * specular_value;
 
-	float diff_value = max(dot(normalized_normal, light_direction), 0);
+	float diff_value = max(dot(normalized_normal, -light_direction), 0);
 	vec3 diffuse = diff_value * light_color;
 
 	float ambient_value = 0.1;
