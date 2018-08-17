fcdb740bd42a2beb862e6c7151ed7d4b3afc3b9b
Deadvard <edvardleiborn@telia.com>
2018-08-17 15:51:43 +0200

    blinn phong

--- BTH COMP GRAPHICS/shaders/phong.fs
+++ BTH COMP GRAPHICS/shaders/phong.fs
@@ -14,22 +14,22 @@ uniform vec3 view_position; // World cordinates
 void main()
 {
 	vec3 normalized_normal = normalize(normal);
-	vec3 light_direction = normalize(fragment_pos - light_pos);
+	vec3 light_direction = normalize(light_pos - fragment_pos);
+    vec3 view_direction = normalize(view_position - fragment_pos);
+	vec3 reflection_direction = reflect(light_direction, normalized_normal);
 
-	vec3 view_direction = normalize(view_position - fragment_pos);
-	vec3 reflection_direction = reflect(light_direction, normalized_normal); //Change direction
 
-	float specular_value = 0.5;
-	float spec = pow(max(dot(view_direction, reflection_direction), 0.0), 32);
-	vec3 specular = light_color * spec * specular_value;
+    vec3 halfway_direction = normalize(light_direction + view_direction);
+    float spec = pow(max(dot(normalized_normal, halfway_direction), 0.0), 256);
+    vec3 specular = light_color * spec;
 
-	float diff_value = max(dot(normalized_normal, -light_direction), 0);
+
+	float diff_value = max(dot(normalized_normal, light_direction), 0);
 	vec3 diffuse = diff_value * light_color;
 
-	float ambient_value = 0.1;
-	vec3 ambient = light_color * ambient_value;
+
+	vec3 ambient = light_color * 0.1; // 0.1 is ambient value
 
 	vec3 result = (ambient + diffuse + specular) * obj_color;
-
 	frag_color = vec4(result, 1);
 }
