f7b8ef4831fd149e437e7b1b0bd415c26e8aa081
OlleNassen <olle_sz@hotmail.com>
2018-08-21 10:14:55 +0200

    removed wireframe

--- BTH COMP GRAPHICS/shaders/tess.es
+++ BTH COMP GRAPHICS/shaders/tess.es
@@ -4,8 +4,6 @@ layout(triangles, equal_spacing, cw) in;
 
 in vec3 cs_position[];
 
-out vec3 patch_distance;
-
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
@@ -16,7 +14,6 @@ void main()
     vec3 p1 = gl_TessCoord.y * cs_position[1];
     vec3 p2 = gl_TessCoord.z * cs_position[2];
 
-    patch_distance = gl_TessCoord;
     vec3 position = normalize(p0 + p1 + p2);
     gl_Position = projection * view * model * vec4(position, 1);
 }
--- BTH COMP GRAPHICS/shaders/tess.fs
+++ BTH COMP GRAPHICS/shaders/tess.fs
@@ -1,25 +1,10 @@
 #version 430
 
-in vec3 geo_patch_distance;
-in vec3 geo_tri_distance;
-
 out vec4 frag_color;
 
 uniform vec3 color;
 
-float amplify(float d, float scale, float offset)
-{
-    d = scale * d + offset;
-    d = clamp(d, 0, 1);
-    d = 1 - exp2(-2*d*d);
-    return d;
-}
-
 void main()
 {
-    float d1 = min(min(geo_tri_distance.x, geo_tri_distance.y), geo_tri_distance.z);
-    float d2 = min(min(geo_patch_distance.x, geo_patch_distance.y), geo_patch_distance.z);
-    vec3 new_color = amplify(d1, 40, -0.5) * amplify(d2, 60, -0.5) * color;
-	
-    frag_color = vec4(new_color, 1.0);
+    frag_color = vec4(color, 1.0);
 }
